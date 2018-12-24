3e97e81ddb1816ae8dfbc76f2abe06fbdab9ae6c
sergiorock <lopezer.nic@gmail.com>
2018-12-24 00:55:55 -0300

    set devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_devise_params, if: :devise_controller?
+
+  def set_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
