cd8e9dee954d90fc85410a5c37e88636ddf4c66c
sergiorock <lopezer.nic@gmail.com>
2018-12-23 23:58:48 -0300

    add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_073518) do
+ActiveRecord::Schema.define(version: 2018_12_24_024532) do
 
   create_table "images", force: :cascade do |t|
     t.string "description"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2018_12_19_073518) do
     t.string "picture_content_type"
     t.bigint "picture_file_size"
     t.datetime "picture_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
